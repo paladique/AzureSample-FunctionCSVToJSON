adebe54439fa743d87ed4b6625d26b9699d01d62
Jasmine G <paladique@users.noreply.github.com>
2017-12-15 16:04:16 -0500

    use CSVHelper

--- CSVConverter/JSONConverter.cs
+++ CSVConverter/JSONConverter.cs
@@ -1,18 +1,16 @@
-using System;
-using System.IO;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Host;
 using Newtonsoft.Json;
+using System.IO;
 using System.Linq;
-using System.Collections.Generic;
-using System.Dynamic;
+using CsvHelper;
 
 namespace CSVConverter
 {
     public static class JSONConverter
     {
         [FunctionName("JSONConverter")]
-        public static void Run([BlobTrigger("to-convert/{name}", Connection = "")]Stream myBlob, string name, TraceWriter log)
+       public static void Run([BlobTrigger("to-convert/{name}", Connection = "")]Stream myBlob, string name, TraceWriter log)
         {
             log.Info($"C# Blob trigger function Processed blob\n Name:{name} \n Size: {myBlob.Length} Bytes");
 
@@ -26,34 +24,21 @@ namespace CSVConverter
             {
                 log.Info("Not a CSV");
             }
+
+           
         }
 
         public static string Convert(Stream blob)
         {
-            string[] strCSV;
+            var t = new StreamReader(blob);
+            var csv = new CsvReader(t);
 
-            using (StreamReader reader = new StreamReader(blob))
-            {
-                strCSV = reader.ReadToEnd().Split(Environment.NewLine.ToCharArray());
-            }
+            csv.Read();
+            csv.ReadHeader();
 
-            //Assume first row is object properties/csv header
-            var properties = strCSV[0].Split(',');
-            var jsonList = new List<dynamic>();
+            var file = csv.GetRecords<object>().ToList();
            
-            foreach (var item in strCSV.Skip(1))
-            {
-                dynamic expando = new ExpandoObject();
-
-                var values = item.Split(',');
-                for (int i = 0; i < properties.Length; i++)
-                {
-                    ((IDictionary<string, object>)expando)[properties[i]] = values[i] ?? "";
-                }
-                jsonList.Add(expando);
-            }
-
-            return JsonConvert.SerializeObject(jsonList);
+            return JsonConvert.SerializeObject(file);
         }
     }
 }
